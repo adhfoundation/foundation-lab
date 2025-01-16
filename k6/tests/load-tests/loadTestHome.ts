6317b281c9a0c7ec64d0a30f51587b8c45516de8
Kaue Bariani <kaue.bariani@iclinic.com.br>
2025-01-16 09:43:59 -0300

    run k6 tests

--- k6/tests/load-tests/loadTestHome.ts
+++ k6/tests/load-tests/loadTestHome.ts
@@ -3,20 +3,20 @@ import { check, sleep } from 'k6';
 
 export const options = {
     vus: 10, // Número de usuários virtuais simultâneos
-    duration: '30s', // Tempo de execução do teste
+    duration: '20s', // Tempo de execução do teste
 };
 
 export default function () {
     // Acessa a página principal
     const resHome = http.get('https://afya.com.br');
     check(resHome, {
-        'Página inicial - status 200': (r) => r.status === 200,
+        'Página inicial - status 200': (r) => r.status === 400,
     });
 
     // Acessa a página "Quem Somos"
     const resQuemSomos = http.get('https://www.afya.com.br/sobre/quem-somos');
     check(resQuemSomos, {
-        'Quem Somos - status 200': (r) => r.status === 200,
+        'Quem Somos - status 200': (r) => r.status === 400,
     });
 
     sleep(1);
