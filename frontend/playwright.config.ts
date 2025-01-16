a9d128c196fa325ce50c5bd96734ba30de59833c
Kaue Bariani <kaue.bariani@iclinic.com.br>
2025-01-16 15:15:03 -0300

    Frontend Tests

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
     use: {
-        headless: false, // Mostra o navegador
+        headless: true, // Mostra o navegador
         viewport: { width: 1280, height: 720 }, // Tamanho da janela do navegador
         browserName: 'chromium', // 'chromium', 'firefox' ou 'webkit'
     },
