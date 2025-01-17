cb157f4476a396c72efa97ce44102b87aedc6894
Kaue Bariani <kaue.bariani@iclinic.com.br>
2025-01-17 11:16:55 -0300

    Frontend tests

--- frontend/src/tests/homePageAfya.spec.ts
+++ frontend/src/tests/homePageAfya.spec.ts
@@ -3,22 +3,16 @@ import { test, expect } from '@playwright/test';
 test('check Afya homepage title', async ({ page }) => {
   await page.goto('https://afya.com.br');
   const title = await page.title();
-  await page.waitForTimeout(2000);
   //expect(title).toBe('Afya | Home');
 
   const menuAfya = page.locator('xpath=//div[@class="text-menu" and text()="Afya"]');
-  await page.waitForTimeout(2000);
   await menuAfya.click();
 
   const menuQuemSomos = page.locator('xpath=//div[@class="text-size-regular" and text()="Quem somos"]');
-  await page.waitForTimeout(2000);
   await menuQuemSomos.click();
 
   const titleCodigoConduta = page.locator('h2.uui-heading-medium-5:text("Código de Conduta Afya")');
-  await page.waitForTimeout(2000);
+  await expect(titleCodigoConduta).toBeVisible();
   const textoTitulo = await titleCodigoConduta.textContent();
   expect(textoTitulo?.trim()).toBe('Código de Conduta Afya');
-
-  await page.waitForTimeout(3000);
-
 });
